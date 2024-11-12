a2f0703bcad432148aaeec16ddabfeb8ec2bf7a1
goutamoff7 <goutamoff7@gmail.com>
2024-11-12 21:06:04 +0530

    Basic Spring Security with role based authentiction and InMemoryUserDetailsManager

---
+++ springFramework/springSecurity/SpringSecurityJwt/src/main/java/com/goutam/SpringSecurityJwt/controller/GreetingsController.java
@@ -0,0 +1,25 @@
+package com.goutam.SpringSecurityJwt.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello";
+    }
+
+    @GetMapping("/user")
+    public String userEndpoint() {
+        return "Hello User!";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminEndpoint() {
+        return "Hello Admin";
+    }
+}

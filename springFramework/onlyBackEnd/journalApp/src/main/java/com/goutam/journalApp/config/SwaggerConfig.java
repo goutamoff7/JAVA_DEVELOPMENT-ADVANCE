ded7d62a344406641375982557034f1c6f14555a
goutamoff7 <goutamoff7@gmail.com>
2025-02-10 16:13:18 +0530

    swagger configuration added for API documentation

---
+++ springFramework/onlyBackEnd/journalApp/src/main/java/com/goutam/journalApp/config/SwaggerConfig.java
@@ -0,0 +1,51 @@
+package com.goutam.journalApp.config;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customSwaggerConfig() {
+        OpenAPI openAPI = new OpenAPI();
+        return openAPI
+                .info(
+                        new Info()
+                                .title("Journal App APIs")
+                                .description("By Goutam")
+                )
+                .servers(
+                        List.of(
+                                new Server().url("http://localhost:8080").description("local"),
+                                new Server().url("http://localhost:8081").description("live"))
+                )
+                .tags(Arrays.asList(
+                                new Tag().name("Admin APIs"),
+                                new Tag().name("Journal Entry APIs"),
+                                new Tag().name("User APIs"),
+                                new Tag().name("Oauth2.0 Login API"),
+                                new Tag().name("Public APIs")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+
+                .components(new Components().addSecuritySchemes(
+                        "bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")
+                ));
+    }
+}

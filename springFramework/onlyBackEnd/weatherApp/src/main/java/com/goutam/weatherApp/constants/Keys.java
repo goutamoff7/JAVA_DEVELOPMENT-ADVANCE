6c08cfa98dc885de071644aad5b6f69a0e53c6f8
goutamoff7 <goutamoff7@gmail.com>
2025-03-16 12:21:54 +0530

    Placeholder added to replace city name and API key

---
+++ springFramework/onlyBackEnd/weatherApp/src/main/java/com/goutam/weatherApp/constants/Keys.java
@@ -0,0 +1,5 @@
+package com.goutam.weatherApp.constants;
+
+public enum Keys{
+    WEATHER_API;
+}
---
+++ springFramework/onlyBackEnd/weatherApp/src/main/java/com/goutam/weatherApp/constants/Placeholder.java
@@ -0,0 +1,6 @@
+package com.goutam.weatherApp.constants;
+
+public interface Placeholder {
+    String CITY ="<CITY>";
+    String API_KEY="<API_KEY>";
+}

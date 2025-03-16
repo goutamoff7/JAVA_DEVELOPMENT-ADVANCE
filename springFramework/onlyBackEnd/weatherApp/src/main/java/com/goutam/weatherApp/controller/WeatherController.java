fadc29e0e1ce9951ea301ca8416d427c43f90206
goutamoff7 <goutamoff7@gmail.com>
2025-03-16 12:30:33 +0530

    refractor:RequestParam added

--- springFramework/onlyBackEnd/weatherApp/src/main/java/com/goutam/weatherApp/controller/WeatherController.java
+++ springFramework/onlyBackEnd/weatherApp/src/main/java/com/goutam/weatherApp/controller/WeatherController.java
@@ -5,29 +5,28 @@ import com.goutam.weatherApp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/weather")
 public class WeatherController {
 
     @Autowired
     private WeatherService weatherService;
-
-    @GetMapping("/city/{city}")
-    public ResponseEntity<?> getWeatherByCity(@PathVariable String city) {
+    //http://localhost:8080/get-weather?city=durgapur
+    @GetMapping("get-weather")
+    public ResponseEntity<?> getWeatherByCity(@RequestParam String city) {
         try{
             WeatherResponse weatherResponse = weatherService.getWeatherByCity(city);
-            if (weatherResponse != null)
+            if (weatherResponse != null) {
+                double feelsLike = weatherResponse.getMain().getFeelsLike() - 273.15;
+                System.out.println("Feels Like : "+String.valueOf(feelsLike).substring(0,5)+"°C");
                 return new ResponseEntity<>(weatherResponse, HttpStatus.OK);
+            }
             else
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>("City Not Found",HttpStatus.NOT_FOUND);
         }catch(Exception e)
         {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("City Not Found",HttpStatus.NOT_FOUND);
         }
 
     }

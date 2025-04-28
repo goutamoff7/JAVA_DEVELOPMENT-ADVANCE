cdc3fc92b8e00dcaf651a8bd69beb4a696d1ff13
goutamoff7 <goutamoff7@gmail.com>
2025-04-28 19:03:55 +0530

    application to run

---
+++ springFramework/directConnectionFrontBack/razorpayPaymentGateway/src/main/java/com/goutam/razorpayPaymentGateway/RazorpayPaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.goutam.razorpayPaymentGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazorpayPaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazorpayPaymentGatewayApplication.class, args);
+	}
+
+}

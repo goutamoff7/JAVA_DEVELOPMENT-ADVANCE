e3300d25f6886e184e8fbedb76d71fa49f4a4ff4
goutamoff7 <goutamoff7@gmail.com>
2025-04-29 12:04:44 +0530

    #

---
+++ springFramework/directConnectionFrontBack/razorpayPaymentGateway/src/test/java/com/goutam/razorpayPaymentGateway/RazorpayPaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goutam.razorpayPaymentGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazorpayPaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

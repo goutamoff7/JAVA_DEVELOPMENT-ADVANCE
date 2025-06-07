6dfc4ef475803471cf86a9752321ce31b55b3e87
goutamoff7 <goutamoff7@gmail.com>
2025-06-07 20:15:10 +0530

    First Commit of the demo video stream application

---
+++ springFramework/directConnectionFrontBack/videoStream/src/main/java/com/goutam/videoStream/SpringBootVideoStreammingApplication.java
@@ -0,0 +1,13 @@
+package com.goutam.videoStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVideoStreammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVideoStreammingApplication.class, args);
+	}
+
+}

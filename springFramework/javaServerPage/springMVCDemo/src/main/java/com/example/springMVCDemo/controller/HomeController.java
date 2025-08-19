0e99e65b84070130b1b15ef3e558376cb902cbe0
goutamoff7 <goutamoff7@gmail.com>
2025-08-19 20:22:27 +0530

    Demonstration of JSP using Spring Framework custom configuration

---
+++ springFramework/javaServerPage/springMVCDemo/src/main/java/com/example/springMVCDemo/controller/HomeController.java
@@ -0,0 +1,51 @@
+package com.example.springMVCDemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.springMVCDemo.model.Student;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        System.out.println("home Method called");
+        return "index";
+    }
+
+    // Using ModelAndView
+//    @RequestMapping("/add-student")
+//    public ModelAndView add(@RequestParam("sId") int id,@RequestParam("sName") String name,
+//                            ModelAndView modelAndView) {
+//        Student student = new Student();
+//        student.setsId(id);
+//        student.setsName(name);
+//        modelAndView.addObject("student",student); // this "result" string should be used in result.jsp
+//        modelAndView.setViewName("result"); // result.jsp is the actual view page
+//        return modelAndView;
+//    }
+
+    // Using ModelAttribute with different variable name
+//    @RequestMapping("/add-student")
+//    public String add(@ModelAttribute("std") Student student) { // this "std" string should be used in result.jsp
+//        return "result";  // result.jsp is the actual view page
+//    }
+
+    // variable name is same as result.jsp page
+    @RequestMapping("/add-student")
+    public String add(Student student) { // this "student" string should be used in result.jsp
+        return "result";  // result.jsp is the actual view page
+    }
+
+    // @ModelAttribute can be used on method also to return some dynamic content
+    @ModelAttribute("course")
+    public String courseName(){
+        return "java";
+    }
+
+
+
+
+}
---
+++ springFramework/javaServerPage/springMVCDemo/src/main/webapp/views/result.jsp
@@ -0,0 +1,13 @@
+<%@page language="java" isELIgnored="false"%>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    </body>
+        <h2>Welcome to ${course} World</h2>
+        <h2>Student Id : ${student.sId}</h2>
+        <h2>Student Name : ${student.sName}</h2>
+        <h2>Student Details : ${student}</h2>
+    </body>
+</html>

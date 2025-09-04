cdc24b5e83f3ee9e86ec99fe0cf634d11fca7b0c
goutamoff7 <goutamoff7@gmail.com>
2025-09-04 08:39:56 +0530

    test package added

---
+++ springFramework/directConnectionFrontBack/videoStream/src/test/java/com/goutam/videoStream/SpringBootVideoStreammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goutam.videoStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVideoStreammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
